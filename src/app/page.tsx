b7c84c573486695944b7ab4ab044314713446e37
mdasifmunshe <asif35-2872@diu.edu.bd>
2024-01-21 16:40:08 +0600

    setup NextUI - UI library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@nextui-org/button'
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div>Hello World!</div>
+      <Button color="primary">Click me</Button>
     </main>
   )
 }
